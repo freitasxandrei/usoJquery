358b81c0fcb041d8c259327ce4fa55f4aced6d15
Gahkkkj <101150465+Gahkkkj@users.noreply.github.com>
2022-08-03 19:59:34 -0300

    save

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> freitasxandrei </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+
+<section class="primeiraSecao">
+
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="assets/img/jquery.jpeg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title"> Alteração de propriedade </h5>
+      <p class="card-text"> Ao clicar no botão, o texto será alterado de branco para preto. </p>
+      <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+      <button type="button" class="btn btn-light">Light</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/img/jquery.jpeg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title"> Alteração de propriedade </h5>
+      <p class="card-text"> Ao clicar no botão, o texto será alterado de branco para preto. </p>
+      <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+      <button type="button" class="btn btn-light">Light</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/img/jquery.jpeg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title"> Alteração de propriedade </h5>
+      <p class="card-text"> Ao clicar no botão, o texto será alterado de branco para preto. </p>
+      <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+      <button type="button" class="btn btn-light">Light</button>
+    </div>
+  </div>
+</div>
+
+</section>
+
+</body>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="assets/js/main.js"></script>
+
+</html>
